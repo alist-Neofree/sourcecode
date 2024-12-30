308a2827de42eefa31f249f3cf3a6764d0348d5a
Mmx233 <36563672+Mmx233@users.noreply.github.com>
2024-12-30 16:17:48 +0800

    feat: run aria2 in main entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@ chown -R ${PUID}:${PGID} /opt/alist/
 
 umask ${UMASK}
 
+if [ "$RUN_ARIA2" = "true" ]; then
+  exec su-exec ${PUID}:${PGID} nohup aria2c \
+    --enable-rpc \
+    --rpc-allow-origin-all \
+    --conf-path=/root/.aria2/aria2.conf \
+    >/dev/null 2>&1 &
+fi
+
 if [ "$1" = "version" ]; then
   ./alist version
 else
