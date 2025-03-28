7e1c13dd0840ca6966b6f578fe3b0bfd6d7857ab
jerry-harm <109275116+jerry-harm@users.noreply.github.com>
2025-03-28 19:21:41 +0800

    fix: :bug: (ipfs): fix the list error caused by not proper join path function
    使用更加规范的路径拼接，修复了有中文或符号的路径无法正常访问的问题

--- drivers/ipfs_api/driver.go
+++ drivers/ipfs_api/driver.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/url"
 	"path/filepath"
-	"strings"
 
 	shell "github.com/ipfs/go-ipfs-api"
 
@@ -43,33 +42,32 @@ func (d *IPFS) Drop(ctx context.Context) error {
 }
 
 func (d *IPFS) List(ctx context.Context, dir model.Obj, args model.ListArgs) ([]model.Obj, error) {
-	path := dir.GetPath()
+	ipfsPath := dir.GetPath()
 	switch d.Mode {
 	case "ipfs":
-		path, _ = url.JoinPath("/ipfs", path)
+		ipfsPath = filepath.Join("/ipfs", ipfsPath)
 	case "ipns":
-		path, _ = url.JoinPath("/ipns", path)
+		ipfsPath = filepath.Join("/ipns", ipfsPath)
 	case "mfs":
-		fileStat, err := d.sh.FilesStat(ctx, path)
+		fileStat, err := d.sh.FilesStat(ctx, ipfsPath)
 		if err != nil {
 			return nil, err
 		}
-		path, _ = url.JoinPath("/ipfs", fileStat.Hash)
+		ipfsPath = filepath.Join("/ipfs", fileStat.Hash)
 	default:
 		return nil, fmt.Errorf("mode error")
 	}
-
-	dirs, err := d.sh.List(path)
+	dirs, err := d.sh.List(ipfsPath)
 	if err != nil {
 		return nil, err
 	}
 
 	objlist := []model.Obj{}
 	for _, file := range dirs {
-		gateurl := *d.gateURL.JoinPath("/ipfs/" + file.Hash)
-		gateurl.RawQuery = "filename=" + url.PathEscape(file.Name)
+		gateurl := *d.gateURL.JoinPath("/ipfs/", file.Hash)
+		gateurl.RawQuery = "filename=" + url.QueryEscape(file.Name)
 		objlist = append(objlist, &model.ObjectURL{
-			Object: model.Object{ID: "/ipfs/" + file.Hash, Name: file.Name, Size: int64(file.Size), IsFolder: file.Type == 1},
+			Object: model.Object{ID: file.Hash, Name: file.Name, Size: int64(file.Size), IsFolder: file.Type == 1},
 			Url:    model.Url{Url: gateurl.String()},
 		})
 	}
@@ -78,8 +76,8 @@ func (d *IPFS) List(ctx context.Context, dir model.Obj, args model.ListArgs) ([]
 }
 
 func (d *IPFS) Link(ctx context.Context, file model.Obj, args model.LinkArgs) (*model.Link, error) {
-	gateurl := d.gateURL.JoinPath(file.GetID())
-	gateurl.RawQuery = "filename=" + url.PathEscape(file.GetName())
+	gateurl := d.gateURL.JoinPath("/ipfs/", file.GetID())
+	gateurl.RawQuery = "filename=" + url.QueryEscape(file.GetName())
 	return &model.Link{URL: gateurl.String()}, nil
 }
 
@@ -91,7 +89,7 @@ func (d *IPFS) MakeDir(ctx context.Context, parentDir model.Obj, dirName string)
 	if path[len(path):] != "/" {
 		path += "/"
 	}
-	return d.sh.FilesMkdir(ctx, path+dirName)
+	return d.sh.FilesMkdir(ctx, filepath.Join(path, dirName))
 }
 
 func (d *IPFS) Move(ctx context.Context, srcObj, dstDir model.Obj) error {
@@ -105,16 +103,16 @@ func (d *IPFS) Rename(ctx context.Context, srcObj model.Obj, newName string) err
 	if d.Mode != "mfs" {
 		return fmt.Errorf("only write in mfs mode")
 	}
-	newFileName := filepath.Dir(srcObj.GetPath()) + "/" + newName
-	return d.sh.FilesMv(ctx, srcObj.GetPath(), strings.ReplaceAll(newFileName, "\\", "/"))
+	dstPath := filepath.Join(filepath.Dir(srcObj.GetPath()), newName)
+	return d.sh.FilesMv(ctx, srcObj.GetPath(), dstPath)
 }
 
 func (d *IPFS) Copy(ctx context.Context, srcObj, dstDir model.Obj) error {
 	if d.Mode != "mfs" {
 		return fmt.Errorf("only write in mfs mode")
 	}
-	newFileName := dstDir.GetPath() + "/" + filepath.Base(srcObj.GetPath())
-	return d.sh.FilesCp(ctx, srcObj.GetPath(), strings.ReplaceAll(newFileName, "\\", "/"))
+	newFileName := filepath.Join(dstDir.GetPath(), filepath.Base(srcObj.GetPath()))
+	return d.sh.FilesCp(ctx, srcObj.GetPath(), newFileName)
 }
 
 func (d *IPFS) Remove(ctx context.Context, obj model.Obj) error {
@@ -135,7 +133,7 @@ func (d *IPFS) Put(ctx context.Context, dstDir model.Obj, s model.FileStreamer, 
 	if err != nil {
 		return err
 	}
-	err = d.sh.FilesCp(ctx, "/ipfs/"+outHash, dstDir.GetPath()+"/"+strings.ReplaceAll(s.GetName(), "\\", "/"))
+	err = d.sh.FilesCp(ctx, filepath.Join("/ipfs/", outHash), filepath.Join(dstDir.GetPath(), s.GetName()))
 	return err
 }
 
