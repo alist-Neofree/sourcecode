8ddca1b687b96227bac29c057bc5c43b63a69b1a
New Future <NewFuture@users.noreply.github.com>
2025-03-31 11:50:19 +0000

    feat(driver): update help doc for Azure Blob

--- drivers/azure_blob/meta.go
+++ drivers/azure_blob/meta.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Addition struct {
-	Endpoint      string `json:"endpoint" required:"true" default:"https://<accountname>.blob.core.windows.net/" help:"The full endpoint URL for Azure Storage, including the account name."`
-	AccessKey     string `json:"access_key" required:"true" help:"The access key for Azure Storage, used for authentication."`
-	ContainerName string `json:"container_name" required:"true" help:"The name of the container in Azure Storage (created in the Azure portal)."`
+	Endpoint      string `json:"endpoint" required:"true" default:"https://<accountname>.blob.core.windows.net/" help:"e.g. https://accountname.blob.core.windows.net/. The full endpoint URL for Azure Storage, including the unique storage account name (3 ~ 24 numbers and lowercase letters only)."`
+	AccessKey     string `json:"access_key" required:"true" help:"The access key for Azure Storage, used for authentication. https://learn.microsoft.com/azure/storage/common/storage-account-keys-manage"`
+	ContainerName string `json:"container_name" required:"true" help:"The name of the container in Azure Storage (created in the Azure portal). https://learn.microsoft.com/azure/storage/blobs/blob-containers-portal"`
 	SignURLExpire int    `json:"sign_url_expire" type:"number" default:"4" help:"The expiration time for SAS URLs, in hours."`
 }
 
 var config = driver.Config{
-	Name:        "AzureBlob",
+	Name:        "Azure Blob Storage",
 	LocalSort:   true,
 	CheckStatus: true,
 }
