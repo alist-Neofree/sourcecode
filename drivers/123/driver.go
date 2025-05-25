10f236e8c52bf85cc1c13fb1b140f4436432aa3e
xxxxxfpx <112059437+xxxxxfpx@users.noreply.github.com>
2025-05-25 17:50:05 +0800

    Update driver.go

--- drivers/123/driver.go
+++ drivers/123/driver.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/alist-org/alist/v3/drivers/base"
 	"github.com/alist-org/alist/v3/internal/driver"
-	"github.com/alist-org/alist/v3/internal/errs"
+	// "github.com/alist-org/alist/v3/internal/errs"
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/internal/stream"
 	"github.com/alist-org/alist/v3/pkg/utils"
